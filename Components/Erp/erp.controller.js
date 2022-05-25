b5c1fc7999ab0bbaf352a5f736dee968bc0e33d7
Mayowa <mayority11@gmail.com>
2022-05-25 11:38:20 +0100

    .

--- Components/Erp/erp.controller.js
+++ Components/Erp/erp.controller.js
@@ -3,24 +3,6 @@ import Drugs from '../Drugs/drugs.model.js'
 
 export const assignErp = async (req, res) => {
   const { uid, entryDate, drugs } = req.body
-  console.log(`Assignment : `, drugs)
-
-  /**
-   * - Check that its not less than the amount being assigned !!!
-   * - Reduce the qty !!!
-   *
-   * -- solution
-   * -- select all from drugs, compare amounts, if any is less than the request, decline the request.
-   * 
-   * {
-  _id: new ObjectId("621f9f86c415f55a1484781e"),
-  name: 'chloroquine',
-  quantity: 100,
-  createdAt: 2022-03-02T16:47:02.981Z,
-  updatedAt: 2022-03-02T16:47:02.981Z,
-  __v: 0
-}
-   */
 
   Drugs.find({}, (er, drugz) => {
     let map = drugs.map((x) => {
@@ -31,7 +13,7 @@ export const assignErp = async (req, res) => {
     })
     let include = map.includes(undefined)
     if (include) {
-      res.status(500).json({
+      return res.status(500).json({
         status: 500,
         message:
           "One or more of your items, don't have enough in the inventory to process this request",
@@ -39,30 +21,57 @@ export const assignErp = async (req, res) => {
     } else {
       const newEntry = new Erp({ uid, entryDate, drugs })
       newEntry.save((er, result) => {
-        console.log(er)
         if (er) {
-          return res.status(500).json({
-            message: 'internal server error',
-            status: 500,
+          Erp.findOne({ uid }, (err, _res) => {
+            let _drugs = _res.drugs
+
+            // check that no existing drugs exists within the array.
+            let x = _drugs.filter((_drug) =>
+              drugs.find((drug) => {
+                return drug.drug === _drug.drug
+              }),
+            )
+
+            if (x.length === 0) {
+              // update the uid's array.
+
+              Erp.updateOne({ uid }, { $push: { drugs } }, (er, _data) => {
+                console.log(`Error_`, er)
+                console.log(`Data : `, _data)
+                return res.status(200).json({
+                  message: 'Update successful!',
+                })
+              })
+            } else {
+              return res.status(500).json({
+                message:
+                  'One or more of the drugs has already been assigned to the user!',
+              })
+            }
+          })
+
+          // return res.status(500).json({
+          //   message: 'internal server error!',
+          //   status: 500,
+          // })
+        } else {
+          // console.log('something is happening') ...
+          // update the inventory of the drugs from here as well ...
+          for (let i = 0; i < drugs.length; i++) {
+            const element = drugs[i]
+            Drugs.updateOne(
+              { name: element.drug },
+              { $inc: { quantity: -element.qty } },
+              (err, response) => {
+                console.log('response')
+              },
+            )
+          }
+          res.status(200).json({
+            message: 'Saved Successfully!',
+            status: 200,
           })
         }
-        //   update the inventory of the drugs from here as well
-        for (let i = 0; i < drugs.length; i++) {
-          const element = drugs[i]
-
-          Drugs.updateOne(
-            { name: element.drug },
-            { $inc: { quantity: -element.qty } },
-            (err, response) => {
-              console.log('response')
-            },
-          )
-        }
-
-        res.status(200).json({
-          message: 'Saved Successfully!',
-          status: 200,
-        })
       })
     }
   })
